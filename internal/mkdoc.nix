3125a31bbb138f81f3570ebcb50f2b3162b302fd
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-02-14 22:08:54 -0500

    document mkDoc

--- internal/mkdoc.nix
+++ internal/mkdoc.nix
@@ -1,4 +1,11 @@
 { pkgs }:
+/*
+   mkDoc returns a derivation with the given name whose source code is
+   located at src. The derivation compiles ${src}/${inFile}.tex
+   to create a PDF file which is placed at $out/${outFile}.pdf.
+   mkDoc assumes that the LaTeX source file can be compiled using
+   a standardized set of dependencies.
+*/
 name: src: inFile: outFile:
   with pkgs;
   let deps = [ (texlive.combine { inherit (texlive) scheme-basic amsmath graphics hyperref; }) ];
