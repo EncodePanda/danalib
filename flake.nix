2bfea32de323cc4180277499a6b6538f8e7110ae
EncodePanda <paul.szulc@gmail.com>
2022-03-07 14:12:37 +0100

    Remove dependency on flake-utils

--- flake.nix
+++ flake.nix
@@ -1,21 +1,22 @@
 {
   # Nixpkgs / NixOS version to use.
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs }:
     let
 
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 self.lastModifiedDate;
 
       # System types to support.
-      supportedSystems = utils.lib.defaultSystems;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+      ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = utils.lib.eachSystemMap supportedSystems;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
